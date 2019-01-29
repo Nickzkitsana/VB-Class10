3731fefd087b040e969aecd7ac96cf4ebd782155
kitsana phomgarm <nick7266@hotmail.com>
2019-01-29 15:58:16 +0700

    Complete Ex4

--- Example1_PictureDB/Example4_updateDB/Form1.Designer.vb
+++ Example1_PictureDB/Example4_updateDB/Form1.Designer.vb
@@ -25,106 +25,117 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnEdit = New System.Windows.Forms.Button()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnShowData = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(28, 34)
+        Me.Label1.Location = New System.Drawing.Point(21, 28)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(82, 17)
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Category ID"
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(127, 31)
+        Me.TextBox1.Location = New System.Drawing.Point(95, 25)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(85, 22)
+        Me.TextBox1.Size = New System.Drawing.Size(65, 20)
         Me.TextBox1.TabIndex = 1
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.btnEdit)
         Me.GroupBox1.Controls.Add(Me.TextBox3)
         Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.TextBox2)
         Me.GroupBox1.Controls.Add(Me.Label2)
-        Me.GroupBox1.Location = New System.Drawing.Point(31, 80)
+        Me.GroupBox1.Location = New System.Drawing.Point(23, 65)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(431, 273)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox1.Size = New System.Drawing.Size(323, 222)
         Me.GroupBox1.TabIndex = 2
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Category Detail"
         '
-        'TextBox2
+        'btnEdit
         '
-        Me.TextBox2.Location = New System.Drawing.Point(144, 36)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.ReadOnly = True
-        Me.TextBox2.Size = New System.Drawing.Size(257, 22)
-        Me.TextBox2.TabIndex = 4
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(20, 39)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(106, 17)
-        Me.Label2.TabIndex = 3
-        Me.Label2.Text = "Category Name"
+        Me.btnEdit.Location = New System.Drawing.Point(230, 175)
+        Me.btnEdit.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(70, 28)
+        Me.btnEdit.TabIndex = 7
+        Me.btnEdit.Text = "Edit"
+        Me.btnEdit.UseVisualStyleBackColor = True
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(144, 80)
+        Me.TextBox3.Location = New System.Drawing.Point(108, 65)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TextBox3.Multiline = True
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(257, 118)
+        Me.TextBox3.Size = New System.Drawing.Size(194, 97)
         Me.TextBox3.TabIndex = 6
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(47, 83)
+        Me.Label3.Location = New System.Drawing.Point(35, 67)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(79, 17)
+        Me.Label3.Size = New System.Drawing.Size(60, 13)
         Me.Label3.TabIndex = 5
         Me.Label3.Text = "Description"
         '
-        'Button1
+        'TextBox2
         '
-        Me.Button1.Location = New System.Drawing.Point(307, 215)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(94, 34)
-        Me.Button1.TabIndex = 7
-        Me.Button1.Text = "Edit"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.TextBox2.Location = New System.Drawing.Point(108, 29)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(194, 20)
+        Me.TextBox2.TabIndex = 4
         '
-        'Button2
+        'Label2
         '
-        Me.Button2.Location = New System.Drawing.Point(231, 25)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(231, 34)
-        Me.Button2.TabIndex = 3
-        Me.Button2.Text = "Show Data"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 32)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Category Name"
+        '
+        'btnShowData
+        '
+        Me.btnShowData.Location = New System.Drawing.Point(173, 20)
+        Me.btnShowData.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnShowData.Name = "btnShowData"
+        Me.btnShowData.Size = New System.Drawing.Size(173, 28)
+        Me.btnShowData.TabIndex = 3
+        Me.btnShowData.Text = "Show Data"
+        Me.btnShowData.UseVisualStyleBackColor = True
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(498, 380)
-        Me.Controls.Add(Me.Button2)
+        Me.ClientSize = New System.Drawing.Size(374, 309)
+        Me.Controls.Add(Me.btnShowData)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.GroupBox1.ResumeLayout(False)
@@ -137,10 +148,10 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnEdit As Button
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents Label3 As Label
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Label2 As Label
-    Friend WithEvents Button2 As Button
+    Friend WithEvents btnShowData As Button
 End Class
--- Example1_PictureDB/Example4_updateDB/Form1.vb
+++ Example1_PictureDB/Example4_updateDB/Form1.vb
@@ -6,4 +6,52 @@ Public Class Form1
     Dim constr As String = "Server=(LocalDB)\MSSQLLocalDB;AttachDBFilename=D:\Northwind.mdf"
     Dim conn As New SqlConnection(constr)
 
+    Private Sub btnShowData_Click(sender As Object, e As EventArgs) Handles btnShowData.Click
+        Try
+            conn.Open()
+            Dim sql As String = "select categoryname , description 
+                                 from categories
+                                 where categoryid = " & TextBox1.Text
+
+            Dim cmd As New SqlCommand(sql, conn)
+            Dim adapter As New SqlDataAdapter(cmd)
+            Dim data As New DataSet()
+            adapter.Fill(data, "category")
+
+            TextBox2.Text = data.Tables("category").Rows(0)("categoryname")
+            TextBox3.Text = data.Tables("category").Rows(0)("description")
+            conn.Close()
+        Catch ex As Exception
+            Dim message = String.Format("Error : {0}", ex.Message)
+            MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        Try
+            conn.Open()
+            Dim sql As String = "Update categories
+                             set description = @description
+                             where categoryname = @name"
+            Dim cmd As New SqlCommand(sql, conn)
+            cmd.Parameters.AddWithValue("description", TextBox3.Text)
+            cmd.Parameters.AddWithValue("name", TextBox2.Text)
+
+            If cmd.ExecuteNonQuery = -1 Then
+                MessageBox.Show("ไม่สามารถอัพเดทข้อมูลได้", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                MessageBox.Show("อัพเดทข้อมูลเรียบร้อย", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                TextBox1.Clear()
+                TextBox2.Clear()
+                TextBox3.Clear()
+                TextBox1.Select()
+            End If
+
+            conn.Close()
+        Catch ex As Exception
+            Dim message = String.Format("Error : {0}", ex.Message)
+            MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
 End Class
