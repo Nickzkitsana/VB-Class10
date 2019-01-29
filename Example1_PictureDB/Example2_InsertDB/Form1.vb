67984057d63a6863028be432ccbf128faddd78f6
kitsana phomgarm <nick7266@hotmail.com>
2019-01-29 15:14:13 +0700

    Complete Ex2

--- Example1_PictureDB/Example2_InsertDB/Form1.vb
+++ Example1_PictureDB/Example2_InsertDB/Form1.vb
@@ -17,4 +17,27 @@ Public Class Form1
         conn.Close()
     End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        conn.Open()
+        Dim sql As String = "insert into categories(categoryname,description)
+                            values(@categoryname,@description)"
+        Dim cmd As New SqlCommand(sql, conn)
+
+        cmd.Parameters.AddWithValue("categoryname", TextBox1.Text)
+        cmd.Parameters.AddWithValue("description", TextBox2.Text)
+
+        If cmd.ExecuteNonQuery = -1 Then
+            MessageBox.Show("ไม่สามารถเพิ่มข้อมูลได้", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            MessageBox.Show("สามารถเพิ่มข้อมูลได้", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+
+
+        conn.Close()
+        showData()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        showData()
+    End Sub
 End Class
